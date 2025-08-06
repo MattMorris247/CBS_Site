7936eba89087f17d9094b257ae3fc23381df5a68
Matt Morris <73907438+MattMorris247@users.noreply.github.com>
2025-08-06 15:20:09 -0400

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "963115157"
+    serviceWorkerVersion: "3834708581"
   }
 });
